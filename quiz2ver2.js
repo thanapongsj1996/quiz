43b6b4d10157c5d0e8a48f3c3a5ced66caaad4c6
Thanapong <thanapongsj1996@gmail.com>
2019-03-23 23:18:17 +0700

    fix bug 03 final01

--- quiz2ver2.js
+++ quiz2ver2.js
@@ -1,11 +1,8 @@
 //หลักการคือคิดกรณี array ปกติ รวม กับกรณี reverse array
+//แยก function findResult สำหรับคิด result ออกมา
 
-function containerSize(arr1) {
-    //สร้าง reverse array
-    arr2 = []
-    for (var i = arr1.length-1; i >= 0; i--){
-        arr2.push(arr1[i])
-    }
+function containerSize(arr) {
+
     function forwardArr(arr) {
         peak = [] //ใช้เก็บค่าตำแหน่งขอบสูงสุด
         h = 0 //ใช้เช็คความสูงตำแหน่งใดๆ
@@ -25,12 +22,7 @@ function containerSize(arr1) {
                 hindex = i
             }
         }
-        //หาผลรวมตั้งแต่ต้นจนถึงก่อนขอบสุดท้าย
-        for (var i = 0; i < peak.length - 1; i++) {
-            for (var j = peak[i] + 1; j < peak[i + 1]; j++) {
-                result = result + arr[peak[i]] - arr[j]
-            }
-        }
+        findResult(arr, peak)
         return result
     }
     function backwardArr(arr) {
@@ -52,25 +44,41 @@ function containerSize(arr1) {
                 hindex = i
             }
         }
+        findResult(arr, peak)
+        return result
+    }
+
+    //ใช้สำหรับคำนวณหา result ในทั้ง forward และ backward
+    function findResult(arr, peak) {
         //หาผลรวมตั้งแต่ต้นจนถึงก่อนขอบสุดท้าย
         for (var i = 0; i < peak.length - 1; i++) {
             for (var j = peak[i] + 1; j < peak[i + 1]; j++) {
                 result = result + arr[peak[i]] - arr[j]
             }
         }
+        console.log('peak: ', peak)
         return result
     }
-    forwardResult = forwardArr(arr1)
-    backwardResult = backwardArr(arr2)
+
+
+    //สร้าง reverse array
+    reverseArray = []
+    for (var i = arr.length - 1; i >= 0; i--) {
+        reverseArray.push(arr[i])
+    }
+
+    forwardResult = forwardArr(arr)
+    backwardResult = backwardArr(reverseArray)
     return forwardResult + backwardResult
 }
 
-var ex = [1,0,2,0,5,0,1,0,3,0,1,0,4]
-//var ex = [1,0,1,0,1]
+//var ex = [1,0,2,0,5,0,1,0,3,0,1,0,4]
+//var ex = [1, 0, 1, 0, 1]
 //var ex = [3, 1, 0, 2, 4, 0, 2, 0, 4, 0, 1, 2]
 //var ex = [0, 0, 0]
 //var ex = [3,0,2,0,1]
 //var ex = [1,0,2,0,3]
-//var ex =[3,2,1,2,0,2,1,3,0,1,0,4]
+//var ex = [3,2,1,2,0,2,1,3,0,1,0,4]
 //var ex = [2,0,3,0,2,0,3,0,2]
+//var ex = [5, 0, 1, 0, 4, 2, 5, 4, 6, 2, 4, 1, 2, 1, 2, 0, 4, 1]
 console.log('result: ', containerSize(ex))
