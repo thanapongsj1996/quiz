1308e8478c75bfebdca96d1b1785b7aeb0e9263a
Thanapong <thanapongsj1996@gmail.com>
2019-04-05 16:24:48 +0700

    add quiz2 ver 3

--- quiz2ver3.js
+++ quiz2ver3.js
@@ -5,21 +5,14 @@ function containerSize(arr) {
 
     function forwardArr(arr) {
         peak = [] //ใช้เก็บค่าตำแหน่งขอบสูงสุด
-        h = 0 //ใช้เช็คความสูงตำแหน่งใดๆ
-        hindex = 0 //ใช้เช็คตำแหน่งของความสูงใดๆ
         lastPeak = 0 //ใช้เก็บค่าขอบสูงสุดล่าสุด
-        lastIndex = 0 //ใช้แทนน้ำแหน่งขอบสุดท้าย (peak สุดท้าย)
         result = 0
 
         //หาตำแหน่ง peak
         for (var i = 0; i < arr.length; i++) {
             if (lastPeak <= arr[i] && arr[i] != 0) {
-                h = 0
                 lastPeak = arr[i]
                 peak.push(i)
-            } else {
-                h = arr[i]
-                hindex = i
             }
         }
         findResult(arr, peak)
@@ -27,21 +20,14 @@ function containerSize(arr) {
     }
     function backwardArr(arr) {
         peak = [] //ใช้เก็บค่าตำแหน่งขอบสูงสุด
-        h = 0 //ใช้เช็คความสูงตำแหน่งใดๆ
-        hindex = 0 //ใช้เช็คตำแหน่งของความสูงใดๆ
         lastPeak = 0 //ใช้เก็บค่าขอบสูงสุดล่าสุด
-        lastIndex = 0 //ใช้แทนน้ำแหน่งขอบสุดท้าย (peak สุดท้าย)
         result = 0
 
         //หาตำแหน่ง peak
         for (var i = 0; i < arr.length; i++) {
             if (lastPeak < arr[i] && arr[i] != 0) {
-                h = 0
                 lastPeak = arr[i]
                 peak.push(i)
-            } else {
-                h = arr[i]
-                hindex = i
             }
         }
         findResult(arr, peak)
@@ -50,7 +36,7 @@ function containerSize(arr) {
 
     //ใช้สำหรับคำนวณหา result ในทั้ง forward และ backward
     function findResult(arr, peak) {
-        //หาผลรวมตั้งแต่ต้นจนถึงก่อนขอบสุดท้าย
+        //หาผลรวมระดับน้ำระหว่าง peak 
         for (var i = 0; i < peak.length - 1; i++) {
             for (var j = peak[i] + 1; j < peak[i + 1]; j++) {
                 result = result + arr[peak[i]] - arr[j]
@@ -60,26 +46,21 @@ function containerSize(arr) {
         return result
     }
 
-
-    //สร้าง reverse array
-    reverseArray = []
-    for (var i = arr.length - 1; i >= 0; i--) {
-        reverseArray.push(arr[i])
-    }
-
+    //หาผลลัพธ์
     forwardResult = forwardArr(arr)
-    backwardResult = backwardArr(reverseArray)
+    backwardResult = backwardArr(arr.reverse())
+
     return forwardResult + backwardResult
 }
 
 //var ex = [1,0,2,0,5,0,1,0,3,0,1,0,4]
-// //var ex = [1, 0, 1, 0, 1]
-var ex = [3, 1, 0, 2, 4, 0, 2, 0, 4, 0, 1, 2]
-// //var ex = [0, 0, 0]
+// var ex = [1, 0, 1, 0, 1]
+//var ex = [3, 1, 0, 2, 4, 0, 2, 0, 4, 0, 1, 2]
+//var ex = [0, 0, 0]
 //var ex = [3,0,2,0,1]
-// //var ex = [1,0,2,0,3]
-// //var ex = [3,2,1,2,0,2,1,3,0,1,0,4]
-//var ex = [2,0,3,0,2,0,3,0,2]
+// var ex = [1,0,2,0,3]
+// var ex = [3,2,1,2,0,2,1,3,0,1,0,4]
+// var ex = [2,0,3,0,2,0,3,0,2]
 //var ex = [5, 0, 1, 0, 4, 2, 5, 4, 6, 2, 4, 1, 2, 1, 2, 0, 4, 1]
 console.log('result: ', containerSize(ex))
 
---
+++ twoarrays.js
@@ -0,0 +1,27 @@
+// function myCheck(arr1, arr2) {
+//     var count = 0
+//     for (var item of arr1) {
+//         if (arr2.includes(item)) count += 1
+//     }
+//     return count
+// }
+
+// function myCheck2(arr1, arr2) {
+//     var count = 0
+//     for (var i = 0; i < arr1.length; i++) {
+//         for (var j = 0; j < arr2.length; j++) {
+//             if(arr1[i] == arr2[j]){
+//                 count = count + 1
+//                 break
+//             }
+//         }
+//     }
+//     return count
+// }
+
+// console.log(myCheck2([1, 2, 3, 4, 5, 6], [1, 2, 3, 6]))
+
+A = [1,3,5,2]
+r = A.reverse()
+console.log(A)
+console.log(r)
